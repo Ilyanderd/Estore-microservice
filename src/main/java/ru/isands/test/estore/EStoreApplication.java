f9b4f4ba9f60676dc4b5f1dec79867d16ad2fa75
Ilyanderd <Povetkin_i@mail.ru>
2024-11-27 21:41:47 +0700

    Добавлена Home HTML-страница

--- src/main/java/ru/isands/test/estore/EStoreApplication.java
+++ src/main/java/ru/isands/test/estore/EStoreApplication.java
@@ -5,13 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @OpenAPIDefinition(servers = { @Server(url = "/", description = "EStore Server") })
-public class EStoreApplication {
+public class EStoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EStoreApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EStoreApplication.class);
+	}
 }
